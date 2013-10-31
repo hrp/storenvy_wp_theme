1e65d96a43cc5da7d7eac25c78881be1d60659ed
Brett Wejrowski <brettw@storenvy.com>
2013-10-31 11:10:51 -0700

    new header

--- header.php
+++ header.php
@@ -40,8 +40,8 @@
 	</head>
 
 	<body <?php body_class(); ?>>
-		<a href=<?php bloginfo('url') ?>>
+		<a href=<?php bloginfo('url') ?> class="bg-wrap">
 			<img src=<?php echo header_image(); ?> alt="" class='bg'>
 		</a>
+		<h1 id="header-logo">Storenvy Store Owner Inspiration &amp; Resources</h1>
 		<div id="container">
-			
